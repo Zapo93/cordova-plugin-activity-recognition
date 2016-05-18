70073d7a06b9263c800b55ea9d5af0ffdfd33c7b
Zapo93 <zbekirski@abv.bg>
2016-05-19 01:23:01 +0300

    Added alert for debugging .

--- www/ActivityRecognition.js
+++ www/ActivityRecognition.js
@@ -1,4 +1,5 @@
 window.ActivityRecognition = function(str, callback) {
+    alert("ActivityRecognition !!!");
     cordova.exec(callback, function(err) {
         callback('Nothing to echo.');
     }, "ActivityRecognition", "GetCurrentActivity", [str]);
