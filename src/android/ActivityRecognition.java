0d5558e10a810902fb4d50ce60111e23e4e9fa57
Zapo93 <zbekirski@abv.bg>
2016-05-19 23:49:03 +0300

    WTF ?!

--- src/android/ActivityRecognition.java
+++ src/android/ActivityRecognition.java
@@ -1,11 +1,11 @@
-package cordova-plugin-activity-recognition;
+package cordova-plugin-activity-recognition
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin
+import org.apache.cordova.CallbackContext
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 /**
  * This class echoes a string called from JavaScript.
