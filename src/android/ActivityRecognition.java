62f9a4006e91a5943bb109bc35a746fabcb4e5c5
Zapo93 <zbekirski@abv.bg>
2016-05-19 23:52:10 +0300

    WTFFF!!!

--- src/android/ActivityRecognition.java
+++ src/android/ActivityRecognition.java
@@ -1,11 +1,11 @@
-package cordova-plugin-activity-recognition
+package cordova-plugin-activity-recognition;
 
-import org.apache.cordova.CordovaPlugin
-import org.apache.cordova.CallbackContext
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
 
-import org.json.JSONArray
-import org.json.JSONException
-import org.json.JSONObject
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -25,7 +25,8 @@ public class ActivityRecognition extends CordovaPlugin {
     private void coolMethod(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
-        } else {
+        } 
+        else {
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
