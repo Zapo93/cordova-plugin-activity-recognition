08906eef947650f28c1414419e9400a46f35992e
ZapryanBekirski <zbekirski@abv.bg>
2016-05-21 14:33:40 +0300

    Changed the name of the method and the formal parameters .

--- www/ActivityRecognition.js
+++ www/ActivityRecognition.js
@@ -1,5 +1,9 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "ActivityRecognition", "coolMethod", [arg0]);
+exports.GetActivity = function(arg0, success) {
+    exec(
+		success,
+		function(){alert("Error!");}, 
+		"ActivityRecognition", "GetActivity", [arg0]
+		);
 };
