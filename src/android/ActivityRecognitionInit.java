175da73850111a96c529eb658d14e4bc3773035c
Zapo93 <zbekirski@abv.bg>
2016-05-24 19:41:21 +0300

    Trying to add Play Services API to project .(pt11)

--- src/android/ActivityRecognitionInit.java
+++ src/android/ActivityRecognitionInit.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import android.app.Activity;
+
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
@@ -13,7 +15,7 @@ import com.google.android.gms.common.ConnectionResult;
 
 import com.google.android.gms.location.ActivityRecognition;
 
-public class ActivityRecognitionInit implements ConnectionCallbacks, OnConnectionFailedListener
+public class ActivityRecognitionInit extends Activity implements ConnectionCallbacks, OnConnectionFailedListener
 {
 	public GoogleApiClient mApiClient;
 	private Boolean Connected;
