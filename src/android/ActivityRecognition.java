fa5d6a5bd192cfc77a6d831f1aa5d89eaebc9003
ZapryanBekirski <zbekirski@abv.bg>
2016-06-02 22:06:04 +0300

    Added testing methods for the Activity Recognition Plugin API .

--- src/android/ActivityRecognition.java
+++ src/android/ActivityRecognition.java
@@ -14,23 +14,103 @@ public class ActivityRecognition extends CordovaPlugin {
 	
 	public static ActivityRequestResult Activity;
 	public static ActivityRecognitionInit API = new ActivityRecognitionInit();
+	private Boolean Connected = false;
+	public CallbackContext callback ;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("GetActivity")) {
-            String message = args.getString(0);
-            this.GetActivity(message, callbackContext);
+        callback = callbackContext;
+		if (action.equals("GetActivity"))
+		{
+            this.GetActivity();
             return true;
         }
+		if (action.equals("Connect"))
+		{
+            this.Connect();
+            return true;
+        }
+		if (action.equals("Dissconnect"))
+		{
+            this.Dissconnect();
+            return true;
+        }
+		if (action.equals("StartActivityUpdates"))
+		{
+			int interval = args.getInt(0);
+            this.StartActivityUpdates(interval);
+            return true;
+        }
+		if (action.equals("StopActivityUpdates"))
+		{
+            this.StopActivityUpdates();
+            return true;
+        }
+		
         return false;
     }
 
-    private void GetActivity(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
+    private void GetActivity() 
+	{
+        if (Connected == true)
+		{
+            callback.success();
         } 
-        else {
-            callbackContext.error("Expected one non-empty string argument.");
+        else 
+		{
+            callback.error("Expected one non-empty string argument.");
         }
     }
+	
+	private void Connect() 
+	{
+        if(Connected == false)
+		{
+			Connected = true;
+			callback.success();
+		}
+		else
+		{
+			callback.error();
+		}
+    }
+	
+	private void Dissconnect() 
+	{
+        if(Connected == true)
+		{
+			Connected = false;
+			callback.success();
+		}
+		else
+		{
+			callback.error();
+		}
+    }
+	
+	private void StartActivityUpdates(int interval) 
+	{
+        if(Connected == true)
+		{
+			callback.success(interval);
+		}
+		else
+		{
+			callback.error();
+		}
+    }
+	
+	private void StopActivityUpdates() 
+	{
+        if(Connected == true)
+		{
+			callback.success();
+		}
+		else
+		{
+			callback.error();
+		}
+    }
+	
+	
 }
--- www/ActivityRecognition.js
+++ www/ActivityRecognition.js
@@ -1,9 +1,42 @@
 var exec = require('cordova/exec');
 
-exports.GetActivity = function(arg0, success) {
+exports.GetActivity = function(success) {
     exec(
 		success,
-		function(){alert("Error!");}, 
-		"ActivityRecognition", "GetActivity", [arg0]
+		function(){alert("Error getting activity!");}, 
+		"ActivityRecognition", "GetActivity", []
+		);
+};
+
+
+exports.Connect = function() {
+    exec(
+		function(){alert("Connected !");},
+		function(){alert("Connectivity Error !");}, 
+		"ActivityRecognition", "Connect", []
+		);
+};
+
+exports.Dissconnect = function() {
+    exec(
+		function(){alert("Disconnected !");},
+		function(){alert("Error while Dissconnecting");}, 
+		"ActivityRecognition", "Dissconnect", []
+		);
+};
+
+exports.StartActivityUpdates = function(interval) {
+    exec(
+		function(Interv){alert("Activity Updates Started ! Interval = ",Interv);},
+		function(){alert("Error while Starting activity updates!");}, 
+		"ActivityRecognition", "StartActivityUpdates", [interval]
+		);
+};
+
+exports.StopActivityUpdates = function() {
+    exec(
+		function(){alert("Activity Updates Stopped");},
+		function(){alert("Error while Stopping activity updates !");}, 
+		"ActivityRecognition", "StopActivityUpdates", []
 		);
 };
